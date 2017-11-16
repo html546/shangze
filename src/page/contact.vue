dcedb3460999c07fa1690057731bd22e8f0783c2
html546 <15712859926@163.com>
2017-11-16 16:17:19 +0800

    加入我们

--- src/page/contact.vue
+++ src/page/contact.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>
-      我是联系我们
+  <div class="contact">
+    <div class="contact1">
+      <h3>联系我们</h3>
+      <p>CONTACT US</p>
+      <span>临沂尚泽文化传媒有限公司</span>
+      <span>座机：05398525765        05398367639</span>
+      <span>手机：15265390126        15553997976</span>
+      <span>Q Q：930305602            290536639</span>
+      <span>地址：山东省临沂市兰山区广场世纪城AB单元902室</span>
+    </div>
   </div>
 </template>
 <script>
+import $ from "jquery";
 export default {
-  
-}
+  mounted() {
+    var height1 = $(window).height() - 62;
+    $(".contact").height(height1);
+  }
+};
 </script>
-<style>
-
+<style scoped>
+.contact{
+  background-image: url(../../static/lxwm.png);
+  background-repeat: repeat;
+  background-size: cover;
+  background-position: center;
+}
+.contact1{
+  width: 400px;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -200px;
+  margin-top: -180px;
+}
+.contact1 h3{
+  margin: 0 0 .67em;
+  padding-top: .67em;
+  font-size: 30px;
+}
+.contact1 p{
+  font-size: 16px;
+  margin-bottom: 20px;
+}
+.contact1 span{
+  font-size: 16px;
+  display: block;
+  width: 100%;
+  text-align: left;
+  line-height: 50px;
+}
 </style>
--- src/page/join.vue
+++ src/page/join.vue
@@ -1,13 +1,100 @@
 <template>
- <div>
-     我是加入我们
+ <div class="join">
+     <div class="join1">
+         <h1>加入我们</h1>
+         <p>JOIN US</p>
+         <Row >
+             <i-col :md="8" :lg="8">
+                 <div class="joinus">
+                     <p>Android开发</p>
+                     <p>5k-8k/月 3-5年 5人 临沂 全职</p>
+                 </div>
+             </i-col>
+             <i-col :md="8" :lg="8">
+                 <div class="joinus">
+                     <p>招聘销售总监</p>
+                     <p>5k-8k/月 3-5年 5人 临沂 全职</p>
+                 </div>
+             </i-col>
+             <i-col :md="8" :lg="8">
+                 <div class="joinus">
+                     <p>PHP开发工程师</p>
+                     <p>面议 3-5年 5人 临沂 全职</p>
+                 </div>
+             </i-col>
+         </Row>
+         <Row >
+             <i-col :md="8" :lg="8">
+                 <div class="joinus">
+                     <p>IOS开发工程师</p>
+                     <p>面议 3-5年 5人 临沂 全职</p>
+                 </div>
+             </i-col>
+             <i-col :md="8" :lg="8">
+                 <div class="joinus">
+                     <p>java开发工程师</p>
+                     <p>5k-8k/月 3-5年 1人 临沂 全职</p>
+                 </div>
+             </i-col>
+             <i-col :md="8" :lg="8">
+                 <div class="joinus">
+                     <p>web前端开发</p>
+                     <p>5k-8k/月 3-5年 5人 临沂 全职</p>
+                 </div>
+             </i-col>
+         </Row>
+     </div>
  </div>
 </template>
 <script>
+import $ from "jquery";
 export default {
-
-}
+  mounted() {
+    var height1 = $(window).height() - 62;
+    $(".join").height(height1);
+  }
+};
 </script>
-<style>
-
+<style scoped>
+.join {
+  background-image: url(../../static/jrwm.png);
+  background-repeat: repeat;
+  background-size: cover;
+  background-position: center;
+}
+.join1 {
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  color: #fff;
+}
+.join1 h1 {
+  font-size: 40px;
+  margin: 0 0 0.67em;
+  padding-top: 0.67em;
+}
+.join1 > p {
+  margin-bottom: 50px;
+}
+.join1 div.joinus {
+  width: 80%;
+  height: 140px;
+  background: #23ade5;
+  color: #fff;
+  border: none;
+  margin: 0 auto;
+  outline: none;
+  border-radius: 5px;
+  overflow: hidden;
+  cursor: pointer;
+}
+.joinus p {
+  line-height: 30px;
+}
+.joinus p:nth-of-type(1) {
+  margin-top: 40px;
+}
+.join1 .ivu-row {
+  margin-bottom: 30px;
+}
 </style>
