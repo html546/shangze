208c2b791a7785f6055355d280f8cdad0b5bbb2f
html546 <15712859926@163.com>
2017-11-16 15:08:39 +0800

    修改关于我们

--- src/page/about.vue
+++ src/page/about.vue
@@ -12,6 +12,50 @@
          为客户提供更全面，更优质得服务！我们希望能够更好的为客户及
          其产品在国内、国外市场打开知名度。使国内外企业商能迅速的
          浏览客户网页及信息，为客户带来无限商机。</p>
+         <Row :gutter="20">
+           <i-col :md="8" :lg="8">
+             <img src="../../static/zs.png" alt="">
+             <h4>更为丰富的经验</h4>
+             <p>尚泽网络服务商遍布全国70多个城市，
+               拥有着极其丰富的实战经验，
+               以保证为客户提供更为专业服务。</p>
+           </i-col>
+           <i-col :md="8" :lg="8">
+             <img src="../../static/zs.png" alt="">
+             <h4>更为强大的团队</h4>
+             <p>尚泽网络致力于用户体验做到最优，
+               其由全国IT精英组成，
+               实时跟进客户了解其需求，
+               帮助客户优化诊断网站可能会出现的各种问题</p>
+           </i-col>
+           <i-col :md="8" :lg="8">
+             <img src="../../static/zs.png" alt="">
+             <h4>更为安全的保障</h4>
+             <p>尚泽网络在服务器段部署Symantec的集防病毒
+               和反恶意软件技术于一体的解决方案，
+               7×24小时监控，保证系统正常运营，网站打开迅速。</p>
+           </i-col>
+           <i-col :md="8" :lg="8">
+             <img src="../../static/zs.png" alt="">
+             <h4>更为强劲的实力</h4>
+             <p>一个好的网站，想要快速吸引到您客户的眼球，
+               必须全方位的展现出企业最强最突出的优势，
+               而尚泽网络则能完美的展现出您企业的优势所在。</p>
+           </i-col>
+           <i-col :md="8" :lg="8">
+             <img src="../../static/zs.png" alt="">
+             <h4>更为低廉的成本</h4>
+             <p>与传统的媒体广告相比，企业网站不仅效果好，
+               其成本也较为低廉，在使用时不需要考虑开支问题，
+               专心关注广告效益。</p>
+           </i-col>
+           <i-col :md="8" :lg="8">
+             <img src="../../static/zs.png" alt="">
+             <h4>更为优质的服务</h4>
+             <p>尚泽网络拥有着高素质的客服团队，完善的售后体系，
+               更为方便的与服务商进行沟通交流。</p>
+           </i-col>
+         </Row>
     </div>
   </div>
 </template>
@@ -41,13 +85,21 @@ export default {
   padding-top: .67em;
   font-size: 40px;
 }
-.about1 p{
+.about1>p{
   font-size: 16px;
   padding: 10px 0px;
 }
-.about1 p:nth-child(3){
+.about1>p:nth-child(3){
  font-size: 16px;
  text-align: left;
  text-indent: 32px;
+ margin-bottom: 50px;
+}
+.about1 .ivu-row img{
+  margin: 20px 0px;
+}
+.about1 .ivu-row h4{
+  font-size: 14px;
+  margin: 0px 0px 20px;
 }
 </style>
