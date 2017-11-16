d780c2ff9494b3ddd91d38b872db5aea1d2d44ca
html546 <15712859926@163.com>
2017-11-16 14:12:39 +0800

    添加背景

--- src/page/document.vue
+++ src/page/document.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-      我是案例
+  <div class="document">
+      
   </div>
 </template>
 <script>
+import $ from 'jquery'
 export default {
-  
+  mounted(){
+    var height1 = $(window).height()-62;
+    $('.document').height(height1);
+  }
 }
 </script>
 <style>
-
+ .document{
+   background-image: url(../../static/document.png);
+   background-repeat: repeat;
+   background-size: 100%;
+   background-position: center;
+   width: 100%;
+ }
 </style>
