e234f4cf399e2f16f324e3d42e3f70c88d6fed3c
html546 <15712859926@163.com>
2017-11-16 17:03:08 +0800

    card组件

---
+++ src/components/carder.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+       <Card :bordered="false">
+            <p slot="title">岗位职责</p>    
+            <ul>
+                <li>1．根据产品需求，设计并完成Android端开发工作 </li>
+                <li>2．参与产品需求分析并参与技术实施方案设计 </li>
+                <li>3．负责编写和维护技术文档</li>
+                <li>4．与html5进行交互式移动平台的开发</li>
+            </ul>
+            <p slot="title">任职要求</p>    
+            <ul>
+                <li>1. 专科以上学历，计算机相关专业 </li>
+                <li>2年以上Android软件开发经验,至少3个独立开发并上线的App </li>
+                <li>3. 较强的Java和面向对象语言功能,Android应用程序开发专家 </li>
+                <li>4. 优秀的沟通技巧 </li>
+            </ul>
+       </Card> 
+    </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
