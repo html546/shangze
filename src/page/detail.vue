496b4f09afd62c80b661322896fc44d4efb4adc9
html546 <1776787598@qq.com>
2017-11-29 21:17:21 +0800

    改图片名

--- src/page/detail.vue
+++ src/page/detail.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-      <img src="../assets/anli_03.png" alt="" v-if="this.$route.params.id==='1'" >
-      <img src="../assets/anli_05.png" alt="" v-if="this.$route.params.id==='2'">
-      <img src="../assets/anli_07.png" alt="" v-if="this.$route.params.id==='3'">
-      <img src="../assets/anli_09.png" alt="" v-if="this.$route.params.id==='4'">
+      <img src="../assets/anli03.png" alt="" v-if="this.$route.params.id==='1'" >
+      <img src="../assets/anli05.png" alt="" v-if="this.$route.params.id==='2'">
+      <img src="../assets/anli07.png" alt="" v-if="this.$route.params.id==='3'">
+      <img src="../assets/anli09.png" alt="" v-if="this.$route.params.id==='4'">
   </div>
 </template>
 <script>
