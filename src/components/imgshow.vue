e0b906c9d8c486681968143fdbcda3542358a5f4
html546 <1776787598@qq.com>
2017-11-29 20:08:40 +0800

    抽出组件

--- src/components/imgshow.vue
+++ src/components/imgshow.vue
@@ -2,149 +2,68 @@
   <div class="imgrow">
                 <Row :gutter="16">
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <div class="imgbox">
-                            <div class="imgboxbg"></div>
-                            <router-link to="">
-                                <div class="yan"></div>
-                            </router-link>
-                        </div>
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="001" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="002" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                       <img-show2 src="../../static/bg2.jpg" number="003" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="004" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="005" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="006" demo="水站商城"></img-show2>
                     </i-col>
                 </Row>
                 <Row :gutter="16">
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="008" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="009" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="010" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="011" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="012" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="013" demo="水站商城"></img-show2>
                     </i-col>
                 </Row>
                 <Row :gutter="16">
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="014" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="015" demo="水站商城"></img-show2>
                     </i-col>
                     <i-col :lg="4" :md="6" :sm="8" :xs="12">
-                        <img src="../../static/bg2.jpg" alt="">
-                        <p class="catenate">
-                            <span>模板号 PCB008</span>
-                            <span>案例</span>
-                        </p>
+                        <img-show2 src="../../static/bg2.jpg" number="016" demo="水站商城"></img-show2>
                     </i-col>
                 </Row>
             </div>
 </template>
 <script>
-export default {};
+import imgShow2 from './imgshow2.vue'
+export default {
+    components:{
+        imgShow2
+    }
+};
 </script>
 <style scoped>
 .imgrow {
   width:100%;
   margin: 20px auto;
-}
-.imgrow img{
-    width: 100%;
-}
-.ivu-row{
-    margin-left: 0px!important;
-    margin-right: 0px!important;
-    margin-top: 20px;
-}
-.catenate{
-    position: absolute;
-    bottom: 5px;
-    left: 8px;
-    background: #000;
-    color: #fff;
-    width: 95%;
-}
-.catenate span:nth-child(2){
-    float: right;
-    padding-right: 20px;
 }
 </style>
---
+++ src/components/imgshow2.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="box">
+    <img :src="src" alt="">
+    <p class="catenate">
+        <span>模板号 PCB{{number}}</span>
+        <span>{{demo}}</span>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+    props:['src','number','demo']
+};
+</script>
+<style scoped>
+.imgrow img{
+    width: 100%;
+}
+.box{
+    width: 100%;
+    position: relative;
+    margin-top: 20px;
+}
+.box:hover{
+    background: black;
+    opacity: .5;
+    cursor: pointer;
+}
+.ivu-row{
+    margin-left: 0px!important;
+    margin-right: 0px!important;
+    margin-top: 20px;
+}
+.catenate{
+    position: absolute;
+    bottom: 5px;
+    /* left: 8px; */
+    background: #000;
+    color: #fff;
+    width: 100%;
+}
+.catenate span{
+    padding: 0px 5px 0px;
+}
+.catenate span:nth-child(2){
+    float: right;
+}
+
+</style>
