2ad505b95d1e065982e8d28b1f6a671f1afe98b3
html546 <15712859926@163.com>
2017-11-30 10:17:22 +0800

    修改图片路径

--- src/page/detail.vue
+++ src/page/detail.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-      <img src="../assets/anli03.png" alt="" v-if="this.$route.params.id==='1'" >
-      <img src="../assets/anli05.png" alt="" v-if="this.$route.params.id==='2'">
-      <img src="../assets/anli07.png" alt="" v-if="this.$route.params.id==='3'">
-      <img src="../assets/anli09.png" alt="" v-if="this.$route.params.id==='4'">
+      <img src="static/anli03.png" alt="" v-if="this.$route.params.id==='1'" >
+      <img src="static/anli05.png" alt="" v-if="this.$route.params.id==='2'">
+      <img src="static/anli07.png" alt="" v-if="this.$route.params.id==='3'">
+      <img src="static/anli09.png" alt="" v-if="this.$route.params.id==='4'">
   </div>
 </template>
 <script>
